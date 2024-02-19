3a44f7f46a12fe08d16679db9c391ee237af36c5
udbern <edwinuduakabasi@gmail.com>
2024-02-19 13:13:21 +0100

    made few changes

--- js/menu.js
+++ js/menu.js
@@ -5,16 +5,18 @@ const sideSection = document.querySelector('.sidesection');
 sideSection.style.animation = 'slide-in 0.5s ease-in-out'; // Add animation to sidesection
 
 menuOpen.addEventListener('click', () => {
-  sideSection.style.transition = 'transform 0.5s ease-in-out';
+  sideSection.style.transition = 'transform 0.5s ease-in';
   sideSection.style.transform = 'translateX(0)';
   sideSection.style.display = 'block'; // Display sidsection when menuOpen is clicked
 });
 
 menuClose.addEventListener('click', () => {
-  sideSection.style.transition = 'transform 0.5s ease-in-out';
-  sideSection.style.transform = 'translateX(-400px)';
-  sideSection.style.display = 'none'; // Hide sidsection when menuClose is clicked
+  sideSection.style.transition = 'transform 0.5s ease-out';
+  sideSection.style.transform = 'translateX(0)';
+  sideSection.style.display = 'none'; 
 });
+
+
 
 
 
